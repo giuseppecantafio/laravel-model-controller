0fcc13de02dbb9d3f88c80b2e8fc53e16184a8ba
Giuseppe <giuseppecantafio@hotmail.it>
2022-06-13 16:29:53 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-center">Ciao <i class="fa-brands fa-android"></i></h1>
+</body>
+</html>
